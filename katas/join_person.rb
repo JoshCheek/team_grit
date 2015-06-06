ecf1d0055989e8a1b6d3ee0961475782b5b8879a
Rachel Parri <erachelparri@gmail.com>
2015-06-06 17:03:43 -0600

    Add join_person kata

---
+++ katas/join_person.rb
@@ -0,0 +1,17 @@
+class Person
+  attr_reader :last_name
+
+  def initialize(first, last)
+    @apple = first
+    @last_name = last
+  end
+
+  def to_s
+    "#{@apple} #{last_name}"
+  end
+end
+
+[Person.new("Josh", "Cheek"),
+Person.new("Rachel", "Parri"),
+Person.new("Rebekah", "Sosa")
+].join(",")
