f96ec894300da2cfd7095e514ebd3a0c7d739334
Josh Cheek <josh.cheek@gmail.com>
2015-07-02 15:49:28 -0600

    Add an example for chess

---
+++ examples/chess.rb
@@ -0,0 +1,110 @@
+class Game
+  def initialize(input, output)
+    @input = input
+    @output = output
+  end
+
+  def play
+    setup the board
+    for every move
+      print the board
+      tell the player to take their turn
+      get the turn from the user
+      print the available moves
+      ask the user where to move the piece to
+      receive the input move
+      print an empty line
+      move the piece
+      tell the player that their move was executed
+      print an empty line
+    print the board
+    print an empty line
+    print game over, and who wins
+  end
+end
+
+Game.new($stdin, $stdout).play
+
+__END__
+$ ruby chess.rb
+
+"8  ♜  ♞  ♝  ♛  ♚  ♝  ♞  ♜\n" +
+"7  ♟  ♟  ♟  ♟  ♟  ♟  ♟  ♟\n" +
+"6                        \n" +
+"5                        \n" +
+"4                        \n" +
+"3                        \n" +
+"2  ♙  ♙  ♙  ♙  ♙  ♙  ♙  ♙\n" +
+"1  ♖  ♘  ♗  ♕  ♔  ♗  ♘  ♖\n" +
+"   a  b  c  d  e  f  g  h\n" +
+"white's turn.\n" +
+"white, your move? " +
+"moves for white pawn f2: f3, f4\n" +
+"white, move f2 where? " +
+"\n" +
+"Ok, white's pawn f2 to f3.\n" +
+"\n" +
+
+"8  ♜  ♞  ♝  ♛  ♚  ♝  ♞  ♜\n" +
+"7  ♟  ♟  ♟  ♟  ♟  ♟  ♟  ♟\n" +
+"6                        \n" +
+"5                        \n" +
+"4                        \n" +
+"3                 ♙      \n" +
+"2  ♙  ♙  ♙  ♙  ♙     ♙  ♙\n" +
+"1  ♖  ♘  ♗  ♕  ♔  ♗  ♘  ♖\n" +
+"   a  b  c  d  e  f  g  h\n" +
+"black's turn.\n" +
+"black, your move? " +
+"moves for black pawn e7: e5, e6\n" +
+"black, move e7 where? " +
+"\n" +
+"Ok, black's pawn e7 to e5.\n" +
+"\n" +
+
+"8  ♜  ♞  ♝  ♛  ♚  ♝  ♞  ♜\n" +
+"7  ♟  ♟  ♟  ♟     ♟  ♟  ♟\n" +
+"6                        \n" +
+"5              ♟         \n" +
+"4                        \n" +
+"3                 ♙      \n" +
+"2  ♙  ♙  ♙  ♙  ♙     ♙  ♙\n" +
+"1  ♖  ♘  ♗  ♕  ♔  ♗  ♘  ♖\n" +
+"   a  b  c  d  e  f  g  h\n" +
+"white's turn.\n" +
+"white, your move? " +
+"moves for white pawn g2: g3, g4\n" +
+"white, move g2 where? " +
+"\n" +
+"Ok, white's pawn g2 to g4.\n" +
+"\n" +
+
+
+"8  ♜  ♞  ♝  ♛  ♚  ♝  ♞  ♜\n" +
+"7  ♟  ♟  ♟  ♟     ♟  ♟  ♟\n" +
+"6                        \n" +
+"5              ♟         \n" +
+"4                    ♙   \n" +
+"3                 ♙      \n" +
+"2  ♙  ♙  ♙  ♙  ♙        ♙\n" +
+"1  ♖  ♘  ♗  ♕  ♔  ♗  ♘  ♖\n" +
+"   a  b  c  d  e  f  g  h\n" +
+"black's turn.\n" +
+"black, your move? " +
+"moves for black queen d8: h4, g5, f6, e7\n" +
+"black, move d8 where? " +
+"\n" +
+"Ok, black's queen d8 to h4.\n" +
+"\n" +
+
+"8  ♜  ♞  ♝     ♚  ♝  ♞  ♜\n" +
+"7  ♟  ♟  ♟  ♟     ♟  ♟  ♟\n" +
+"6                        \n" +
+"5              ♟         \n" +
+"4                    ♙  ♛\n" +
+"3                 ♙      \n" +
+"2  ♙  ♙  ♙  ♙  ♙        ♙\n" +
+"1  ♖  ♘  ♗  ♕  ♔  ♗  ♘  ♖\n" +
+"   a  b  c  d  e  f  g  h\n" +
+"\n" +
+"Checkmate, black wins.\n"
